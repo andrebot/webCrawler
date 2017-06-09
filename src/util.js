10edbc6ff75ebd24150bf8d46f6fdc6f2e3e6b3a
Andre Botelho <andrebot_almeida@hotmail.com>
2017-06-09 20:33:40 -0300

    testing extract Domain

--- src/util.js
+++ src/util.js
@@ -5,9 +5,23 @@ const Util = {
 };
 
 function extractHostname (url) {
+  let hostname = '';
 
+  if (url.indexOf('http') > -1) {
+    hostname = url.split('://')[1];
+  }
+
+  if (hostname.indexOf('www') > -1) {
+    hostname = hostname.split('w.')[1];
+  }
+
+  if (hostname.indexOf('/') > -1) {
+    hostname = hostname.split('/')[0];
+  }
+
+  return hostname;
 }
 
 module.exports = function Factory () {
-  return Object.assing({}, Util);
+  return Object.assign({}, Util);
 };
--- src/util.spec.js
+++ src/util.spec.js
@@ -2,7 +2,22 @@
 
 const Cheerio = require('cheerio');
 const should  = require('chai').should();
+const Util    = require('./util');
 
 describe('Utils', function () {
+  const DUMMY_URL = 'http://www.avenuecode.com/events';
 
+  before(function () {
+    this.util = Util();
+  });
+
+  it('Should be able to extract the hostname of an url', function () {
+    const hostname = this.util.extractHostname(DUMMY_URL);
+
+    should.exist(hostname);
+    hostname.should.not.be.empty;
+    hostname.indexOf('http://').should.be.eq(-1);
+    hostname.indexOf('www.').should.be.eq(-1);
+    hostname.indexOf('/').should.be.eq(-1);
+  });
 });
