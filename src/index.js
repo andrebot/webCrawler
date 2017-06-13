b9ad9d18bb37e8aae7fd36f3b0f98afb8308ab03
André Botelho Almeida <almeidaa@ae.com>
2017-06-13 18:07:16 -0300

    Adding index file to execute the code

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@
 
 const Crawler = require('./crawler');
 
+const _variable = 'url=';
 const avenueCodeCrawler = Crawler();
 
-avenueCodeCrawler.crawlPages('http://www.avenuecode.com').then(function (data) {
+const urlToUse = process.argv.reduce(function (url, parameter) {
+  if (parameter.indexOf(_variable) > -1) {
+    return parameter.split(_variable)[1];
+  } else {
+    return url;
+  }
+}, null);
+
+if (!urlToUse) {
+  throw new Error('No URL was provided. Please run the code with "url=<your_url>" as a parameter');
+}
+
+avenueCodeCrawler.crawlPages(urlToUse).then(function (data) {
   console.log(data);
 }).catch(console.error);
