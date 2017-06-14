c024a74039dc8376a07ccb04007085da82325335
André Botelho Almeida <almeidaa@ae.com>
2017-06-14 17:59:16 -0300

    Better handle of the query parameter

--- src/index.js
+++ src/index.js
@@ -11,11 +11,13 @@ if (cluster.isMaster) {
     if (parameter.indexOf(_startingUrl) > -1) {
       validParameters.url = parameter.split(_startingUrl)[1];
     } else if (parameter.indexOf(_queryParameter) > -1) {
-      validParameters.query = parameter.split(_queryParameter)[1];
+      validParameters.query = parameter.split(_queryParameter)[1] === 'true';
     }
 
     return validParameters;
   }, {query: false});
+
+  console.log(parameters);
 
   if (!parameters.url) {
     throw new Error('No URL was provided. Please re-run with "url=<your_url>" as a parameter');
